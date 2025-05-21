eb2e6ab56f2a627464becf24070802c65447e8ab
LeeHunJun <plaskmaster@naver.com>
2025-05-21 22:14:52 +0900

    UserTamagotchiServie.cpp 올립니다.

--- PokeTODO/src/models/Tamagotchi.h
+++ PokeTODO/src/models/Tamagotchi.h
@@ -1,7 +1,8 @@
-﻿/*
-#pragma once
+﻿#pragma once
 #include <string>
 #include "TamagotchiState.h"
+
+class TamagotchiState;  // 전방 선언(포인터만 사용하므로 가능)
 
 class Tamagotchi {
 private:
@@ -16,11 +17,15 @@ public:
     Tamagotchi(int id, const std::string& pokeName);
     ~Tamagotchi();
 
-    // State management
+    // 복사 생성자와 복사 대입 연산자 삭제 (포인터 관리 때문에)
+    Tamagotchi(const Tamagotchi&) = delete;
+    Tamagotchi& operator=(const Tamagotchi&) = delete;
+
+    // 상태 관리
     void setState(TamagotchiState* newState);
     std::string getCurrentStateName() const;
 
-    // Actions
+    // 행동
     void feed();
     void play();
     void increaseHunger();
@@ -28,7 +33,7 @@ public:
     void levelUp();
     void updateStatus();
 
-    // Getters
+    // getter
     int getPokeId() const;
     std::string getName() const;
     int getLevel() const;
@@ -36,9 +41,8 @@ public:
     int getHunger() const;
     TamagotchiState* getCurrentState() const;
 
-    // Setters
+    // setter
     void setName(const std::string& newName);
     void setHappiness(int value);
     void setHunger(int value);
-}; 
-*/ 
+};
